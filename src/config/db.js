7bf4ee92f242299fe71dc53adf4f872887356f12
Vignesh <v97vignesh97@gmail.com>
2025-03-27 23:23:07 +0530

    All updated successfully

--- src/config/db.js
+++ src/config/db.js
@@ -5,11 +5,12 @@ dotenv.config();
 
 const connectDB = async()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     }
-    catch{
-        console.log('error',error);
+    catch(err){
+        console.log(error);
+        process.exit(1);
     }
 };
 
